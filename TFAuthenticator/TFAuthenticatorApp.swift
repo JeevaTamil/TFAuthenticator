3bfc59710bce89135a01f4a88b04293ea321756e
Azhagusundaram.t <Azhagusundaram.t@zohocorp.com>
2021-06-14 18:37:44 +0530

    Initial Commit

---
+++ TFAuthenticator/TFAuthenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TFAuthenticatorApp.swift
+//  TFAuthenticator
+//
+//  Created by Azhagusundaram Tamil on 14/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TFAuthenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
